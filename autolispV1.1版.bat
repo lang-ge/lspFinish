17ec3ee70dd444fe5d2724f5c0048634dd66e743
lang ge <zhenyoudong1@hotmail.com>
2020-03-11 11:19:56 +0800

    Update 1.1

--- autolispV1.1版.bat
+++ autolispV1.1版.bat
@@ -11,22 +11,23 @@ Else
 		FileName = "{(}load """ & Replace(arg, "\", "/") & """{)}~" 
 	Next   
 End If
-Dim ws
+Dim ws,App
 Set ws = CreateObject("wscript.shell")
-If IsProcess("acad.exe") = True Then
+Set App = GetObject(, "AutoCAD.Application")
+If App Then
 	ws.appactivate "AutoCAD 2008":ws.SendKeys "% R":WScript.Sleep 50:ws.SendKeys FileName '"3z "
 Else
 	ws.Run """D:\Program Files\AutoCAD 2008\acad.exe""", 3:WScript.Sleep 3000:ws.SendKeys " ":WScript.Sleep 50:ws.SendKeys FileName '"3z "
 End If
 
 '检查当前进程列表中是否存在指定的进程2
-Function IsProcess(strProcessName)
-	
-	Dim objWMIService
-	Set objWMIService = GetObject("winmgmts:\\.\root\cimv2")
-	Dim colProcessList
-	Set colProcessList = objWMIService.ExecQuery("SELECT * FROM Win32_Process WHERE Name='" & strProcessName & "'")
-	IsProcess = CBool(colProcessList.Count)
-	
-End Function
+'Function IsProcess(strProcessName)
+'	
+'	Dim objWMIService
+'	Set objWMIService = GetObject("winmgmts:\\.\root\cimv2")
+'	Dim colProcessList
+'	Set colProcessList = objWMIService.ExecQuery("SELECT * FROM Win32_Process WHERE Name='" & strProcessName & "'")
+'	IsProcess = CBool(colProcessList.Count)
+'	
+'End Function
 
